6e51b00afa205bd9226373eacadef2c6a5af4744
quang <quang.hoang@enouvo.com>
2016-11-16 16:40:15 +0700

    Create menu

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Router, Route, Link, browserHistory } from 'react-router';
-import Home from './containers/Home';
+import { Router, Route, browserHistory } from 'react-router';
 import Login from './containers/Login';
+import Home from './containers/Home';
+import Dashboard from './containers/Dashboard';
+import Orders from './containers/Orders';
+import Kitchen from './containers/Kitchen';
 
 class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={Home}/>
         <Route path="/login" component={Login}/>
+        <Route path="/" component={Home}/>
+        <Route path="/dashboard" component={Dashboard}/>
+        <Route path="/orders" component={Orders}/>
+        <Route path="/kitchen" component={Kitchen}/>
       </Router>
     );
   }
--- src/app/containers/Home.js
+++ src/app/containers/Home.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import logo from '../../media/logo.svg';
-import '../../css/App.css';
+import HomeMenu from '../components/HomeMenu';
+import '../../css/Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <HomeMenu />
         </div>
         <p className="Home-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
